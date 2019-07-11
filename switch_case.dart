a368ed5fd63cb0be511065fe86d14c444629dd8c
gautamamber <ambergautam1@gmail.com>
2019-07-11 15:34:33 +0530

    feat(switch case)

---
+++ switch_case.dart
@@ -0,0 +1,18 @@
+void main(){
+  String grade = 'A';
+  switch(grade) {
+    case "A": 
+      print("Excellent grade");
+      break;
+    
+    case "B":
+      print("Good grade");
+      break;
+    
+    case "C":
+      print("Bad");
+      break;
+    default:
+      print("Invalid");
+  }
+}
