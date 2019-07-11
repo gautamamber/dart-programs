cfd4ac85edae00a2dcc6a495bd350582ab71b824
gautamamber <ambergautam1@gmail.com>
2019-07-11 15:13:47 +0530

    feat(data types)

---
+++ data_types.dart
@@ -0,0 +1,33 @@
+void main(List<String> arguments){
+  // numbers
+  int score = 23;
+  var count = 20;
+  int hexValue = 0xEADEBAEE;
+
+  // decimal or double functions
+
+  double percent = 33.3;
+  var percentage = 44.3;
+  double exponents = 1.23e3;
+
+  // strings
+
+  String name = "amber gautam";
+  var address = "new york";
+
+  // boolean 
+  bool isActive = true;
+  bool status = false;
+
+  // print all the datatypes or variables
+
+  print(score);
+  print(count);
+  print(hexValue);
+  print(isActive);
+  print(name);
+  print(status);
+  print(address);
+  print(hexValue);
+
+}
