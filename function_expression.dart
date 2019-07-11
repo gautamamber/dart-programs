2574d36424142771361de460946e20c0fd13679d
gautamamber <ambergautam1@gmail.com>
2019-07-11 16:01:11 +0530

    feat(function expression)

---
+++ function_expression.dart
@@ -0,0 +1,13 @@
+void main(){
+  findPerimeter(3,4);
+  getArea(4, 5);
+  
+}
+
+void findPerimeter(int l, int b) => print("perimeter is ${2*(l+b)}");
+
+void getArea(int l, int b) => print("Area is ${l*b}");
+
+
+// => is a fat arrow
+// => short hand syntax for return expression 
