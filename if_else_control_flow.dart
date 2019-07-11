122c1c670b4f5517d5250bc90080f65a8c48f925
gautamamber <ambergautam1@gmail.com>
2019-07-11 15:27:13 +0530

    feat(if else)

---
+++ if_else_control_flow.dart
@@ -0,0 +1,27 @@
+void main(){
+  var salary = 15000;
+  // paranthesis are mandatory in if else statements
+  if (salary > 20000){
+      print("you got promotions");
+  }
+  else {
+        print("You need to work hard");
+  }
+
+  // if else ladder
+
+  var marks = 60;
+  if (marks>=90 && marks <100){
+    print("A grade");
+  }
+  else if (marks > 80 && marks < 90){
+    print("B grade");
+  }
+  else if (marks > 70 && marks < 80){
+    print("C grade");
+  }
+  else{
+    print("D grade");
+  }
+
+}
