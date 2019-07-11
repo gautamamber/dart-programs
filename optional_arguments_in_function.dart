a3159d8536f5bfcabf4811ec234a3bc302efe5e9
gautamamber <ambergautam1@gmail.com>
2019-07-11 16:04:35 +0530

    feat(function optional argument)

---
+++ optional_arguments_in_function.dart
@@ -0,0 +1,11 @@
+void main(){
+  printList("amber");
+}
+
+printList(String name1, [String name2, String name3]){
+  print("name1 is $name1");
+  print("name2 is $name2");
+  
+}
+
+// optional argument is return null
