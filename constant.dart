aa0e5622dad1dd729efcfdecb7c5225988a55f95
gautamamber <ambergautam1@gmail.com>
2019-07-11 15:22:24 +0530

    feat(constants)

---
+++ constant.dart
@@ -0,0 +1,11 @@
+void main(){
+  final city = "delhi";
+  print(city);
+  // final are constant and cannot be change
+  // city = "mumbai"; throw an error
+
+  const PI = 3.14;
+  // we cannot assign value to a constant variable
+  print(PI);
+
+}
